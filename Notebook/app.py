0a46cf7713ac50159a454431b11b9e7f968fc4f6
Athira <adhimohanachu2002@gmail.com>
2023-12-17 23:26:52 +0530

    Added Flask

--- Notebook/app.py
+++ Notebook/app.py
@@ -1,51 +1,4 @@
-
-# from flask import Flask, render_template, request
-# import pickle
-
-# app = Flask(__name__)
-
-# # Load model
-# model = pickle.load(open('savedmodel.sav', 'rb'))
-
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-
-# @app.route('/Check', methods=['POST'])
-# def Check():
-    
-#     if request.method == 'POST':
-#         # Accept all the values from the form
-#         attributes = [
-#             'A1_Score', 'A2_Score', 'A3_Score', 'A4_Score', 'A5_Score',
-#             'A6_Score', 'A7_Score', 'A8_Score', 'A9_Score', 'A10_Score',
-#             'age', 'gender', 'jaundice', 'austim', 'contry_of_res',
-#             'result'
-#         ]
-
-#         values = []
-#         for attribute in attributes:
-#             if attribute in ['gender', 'jaundice', 'austim']:
-#                 # Assuming binary values where 'yes' is encoded as 1 and 'no' as 0
-#                 values.append(1 if request.form[attribute] == 'yes' else 0)
-#             else:
-#                 # For other attributes, assume they are numeric
-#                 values.append(float(request.form[attribute]))
-
-#         # Make predictions using the model
-#         result = model.predict([values])[0]
-       
-        
-#         print(result)
-      
-#         return result
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
@@ -61,7 +14,7 @@ def preprocess_and_predict(test_data, model):
         'A1_Score', 'A2_Score', 'A3_Score', 'A4_Score', 'A5_Score',
         'A6_Score', 'A7_Score', 'A8_Score', 'A9_Score', 'A10_Score',
         'age', 'gender', 'ethnicity', 'jaundice', 'austim', 'contry_of_res', 'used_app_before',
-        'result', 'age_desc', 'relation', 'Class/ASD'
+        'result', 'age_desc', 'relation'
     ])
 
     # Drop unwanted columns
@@ -102,29 +55,24 @@ def preprocess_and_predict(test_data, model):
 def home():
     return render_template('index.html', result=None)
 
-@app.route('/Check', methods=['POST'])
-def Check():
+@app.route('/predict', methods=['POST'])
+def predict():
     if request.method == 'POST':
         # Accept all the values from the form
         attributes = [
             'A1_Score', 'A2_Score', 'A3_Score', 'A4_Score', 'A5_Score',
             'A6_Score', 'A7_Score', 'A8_Score', 'A9_Score', 'A10_Score',
-            'age', 'gender', 'ethnicity', 'jaundice', 'austim', 'contry_of_res', 'used_app_before',
-            'result', 'age_desc', 'relation'
+            'age', 'gender', 'ethnicity', 'jaundice', 'austim', 'contry_of_res',
+            'used_app_before', 'result', 'age_desc', 'relation'
         ]
 
-        values = []
-        for attribute in attributes:
-            if attribute in ['gender', 'jaundice', 'austim']:
-                values.append(1 if request.form[attribute] == 'yes' else 0)
-            else:
-                values.append(float(request.form[attribute]))
+        values = [request.form[attr] for attr in attributes]
 
         # Make predictions using the model
         result = preprocess_and_predict(values, model)
+        result_label = 'Yes' if result[0] == 1 else 'No'
 
-        # Return the result as JSON
-        return jsonify({'result': 'Yes' if result[0] == 1 else 'No'})
+        return render_template('index.html', result=result_label)
 
 if __name__ == '__main__':
     app.run(debug=True)
